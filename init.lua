1c1a978db58ede3d807dec9a81f85d2b4e825bb5
GeekchanskiY <astrovskiy.04@gmail.com>
2024-04-08 11:09:57 +0200

    upd

---
+++ init.lua
@@ -0,0 +1,19 @@
+require("keymaps")
+o = vim.opt
+
+o.mouse = "a"
+o.shiftwidth = 4
+o.tabstop = 4
+o.number = true
+o.relativenumber = true
+o.fileencoding = "utf-8"
+o.clipboard = "unnamedplus"
+o.cursorline = true
+o.backup = false
+o.ignorecase = true
+o.hlsearch = true
+o.smartindent = true
+o.cmdheight = 2
+o.swapfile = false
+o.wrap = false
+
