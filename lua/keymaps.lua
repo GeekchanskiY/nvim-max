9a45e7ac2a8f845beb20af723c73534d8fa99b5d
GeekchanskiY <astrovskiy.04@gmail.com>
2024-09-16 16:39:46 +0300

    keymaps resize reverse

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -14,8 +14,8 @@ keymap("n", "<C-l>", "<C-w>l", opts)
 
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
 keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize +2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Lexplore --
 keymap("n", "<leader>e", ":Lex 30<cr>", opts)
