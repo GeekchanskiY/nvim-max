e3c985bf8569fe3dd6d479db45e51bd58c0a00c9
GeekchanskiY <astrovskiy.04@gmail.com>
2024-09-17 16:59:39 +0300

    upd

--- init.lua
+++ init.lua
@@ -59,7 +59,7 @@ require'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,
 
-    disable = function(lang, buf)
+    disable = function(_, buf)
         local max_filesize = 10 * 1024 * 1024 -- 10 MB
         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
         if ok and stats and stats.size > max_filesize then
@@ -71,4 +71,62 @@ require'nvim-treesitter.configs'.setup {
   },
 }
 
+-- statusline 
+local cmp = {} -- statusline components
 
+--- highlight pattern
+-- This has three parts: 
+-- 1. the highlight groups
+-- 2. text content
+-- 3. special sequence to restore highlight: %*
+-- Example pattern: %#SomeHighlight#some-text%*
+local hi_pattern = '%%#%s#%s%%*'
+
+function _G._statusline_component(name)
+  return cmp[name]()
+end
+
+function cmp.diagnostic_status()
+  local ok = ' λ '
+
+  local ignore = {
+    ['c'] = true, -- command mode
+    ['t'] = true  -- terminal mode
+  }
+
+  local mode = vim.api.nvim_get_mode().mode
+
+  if ignore[mode] then
+    return ok
+  end
+
+  local levels = vim.diagnostic.severity
+  local errors = #vim.diagnostic.get(0, {severity = levels.ERROR})
+  if errors > 0 then
+    return ' ✘ '
+  end
+
+  local warnings = #vim.diagnostic.get(0, {severity = levels.WARN})
+  if warnings > 0 then
+    return ' ▲ '
+  end
+
+  return ok
+end
+
+function cmp.position()
+  return hi_pattern:format('Search',' %3l:%-2c ')
+end
+
+local statusline = {
+  '%{%v:lua._statusline_component("diagnostic_status")%} ',
+  '%t',
+  '%r',
+  '%m',
+  '%=',
+  '%{&filetype} ',
+  ' %2p%% ',
+  '%{%v:lua._statusline_component("position")%}'
+}
+
+vim.o.statusline = table.concat(statusline, '')
